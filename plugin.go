83885ec69845ebd0c663fe636f8d66ddeba05cd3
ischenkx <formypsnspb@gmail.com>
2023-05-28 13:22:08 +0400

    to be dropped

--- plugin.go
+++ plugin.go
@@ -2,6 +2,7 @@ package kantoku
 
 type Plugin interface {
 	Initialize(kantoku *Kantoku)
+
 	BeforeInitialized(ctx *Context) error
 	AfterInitialized(ctx *Context)
 	BeforeScheduled(ctx *Context) error
