fd2cb1f888d1a4365e86500d44d784c2e924eda6
ischenkx <formypsnspb@gmail.com>
2023-06-04 18:53:48 +0400

    KAN-33 - added an executor entry

---
+++ unused/backend/stand/executor/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/samber/lo"
+	"kantoku"
+	evexec "kantoku/unused/backend/executor/events"
+	"kantoku/unused/backend/stand/common"
+	"log"
+	"strconv"
+)
+
+type Runner struct{}
+
+func (r *Runner) Run(ctx context.Context, task kantoku.TaskInstance) ([]byte, error) {
+	log.Println("Running:", task.ID())
+	switch task.Type {
+	case "factorial":
+		x, err := strconv.Atoi(string(task.Data))
+		if err != nil {
+			return nil, err
+		}
+		return []byte(strconv.Itoa(factorial(x))), nil
+	case "reverse":
+		return lo.Reverse(task.Data), nil
+	}
+
+	return nil, nil
+}
+
+func main() {
+	executor := evexec.Builder[kantoku.TaskInstance]{
+		Runner:   &Runner{},
+		Platform: common.MakePlatform(),
+		Resolver: evexec.ConstantResolver("TEST_STAND_EVENTS"),
+	}.Build()
+
+	log.Println("Starting...")
+	if err := executor.Run(context.Background()); err != nil {
+		log.Println("failed to run:", err)
+	}
+}
+
+func factorial(x int) int {
+	if x <= 1 {
+		return 1
+	}
+	return x * factorial(x-1)
+}
