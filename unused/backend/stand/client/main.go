9e5f8fc68b5e8b7de8554f71ace7a19452a79af3
DenShlk <52719698+DenShlk@users.noreply.github.com>
2023-06-05 23:43:59 +0300

    KAN-33 - updated client/main to show that system actually works

--- unused/backend/stand/client/main.go
+++ unused/backend/stand/client/main.go
@@ -5,18 +5,35 @@ import (
 	"fmt"
 	"kantoku"
 	"kantoku/unused/backend/stand/common"
+	"time"
 )
 
 func main() {
-	result, err := kantoku.New(common.MakePlatform()).Spawn(
+	kan := kantoku.New(common.MakePlatform())
+	result, err := kan.Spawn(
 		context.Background(),
 		kantoku.Task("factorial", []byte("12")),
 	)
 
 	if err != nil {
-		fmt.Println("Failed:", err)
+		fmt.Println("Failed to spawn task:", err)
 		return
 	}
+	fmt.Println("Spawned task:", result.Task)
 
-	fmt.Println("Done:", result.Task)
+	time.Sleep(time.Second * 5)
+	taskView := kan.Task(result.Task)
+	data, err := taskView.Data(context.Background())
+	if err != nil {
+		fmt.Println("Failed to get task data:", err)
+		return
+	}
+	fmt.Println("Data:", string(data))
+
+	output, err := taskView.Result(context.Background())
+	if err != nil {
+		fmt.Println("Failed to get task result:", err)
+		return
+	}
+	fmt.Println("Output:", string(output.Data))
 }
