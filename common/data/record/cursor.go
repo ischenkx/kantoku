79914c39f3702cf808a98cf8aa5850cfcfd3b24c
ischenkx <formypsnspb@gmail.com>
2023-06-30 17:51:35 +0400

    KAN-57 - some comments on masking

--- common/data/record/cursor.go
+++ common/data/record/cursor.go
@@ -5,6 +5,11 @@ import "context"
 type Cursor[Item any] interface {
 	Skip(int) Cursor[Item]
 	Limit(int) Cursor[Item]
+	// Mask is a method that allows dynamically include/exclude some entries by their name
+	//
+	// NOTE: Following operations are equivalent:
+	//     cursor.Mask(masks1...).Mass(masks2...)
+	//     cursor.Mask(append(masks1, masks2...)...)
 	Mask(masks ...Mask) Cursor[Item]
 	Sort(sorters ...Sorter) Cursor[Item]
 	Iter() Iter[Item]
