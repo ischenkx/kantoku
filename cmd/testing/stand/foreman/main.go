ded03d0eb36f3caed5ab5f35bf44f9607bf22a5b
DenShlk <denshlk@yandex.ru>
2024-02-17 21:01:51 +0300

    now demo code is all in stand (task defs + executor creation) added more run configs

--- cmd/testing/stand/foreman/main.go
+++ cmd/testing/stand/foreman/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"context"
 	"github.com/ischenkx/kantoku/cmd/testing/stand"
-	"github.com/ischenkx/kantoku/cmd/testing/stand/common"
 	"github.com/ischenkx/kantoku/pkg/lib/tasks/functional"
 	"github.com/ischenkx/kantoku/pkg/lib/tasks/future"
 )
 
 func main() {
 	ctx := context.Background()
-	sys := stand.NewSystem(ctx)
+	sys := NewSystem(ctx)
 
 	err := functional.SchedulingContext(context.Background(), sys, func(ctx *functional.Context) error {
 		for i := 0; i < 1; i++ {
-			functional.Execute[common.SumTask, common.SumInput, common.MathOutput](ctx, common.SumTask{},
-				common.SumInput{Args: future.FromValue([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})},
+			functional.Execute[stand.SumTask, stand.SumInput, stand.MathOutput](ctx, stand.SumTask{},
+				stand.SumInput{Args: future.FromValue([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})},
 			)
 		}
 
